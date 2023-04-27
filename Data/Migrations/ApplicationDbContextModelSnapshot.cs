98debe1e45574b25cf7885f7590ca6352910eb1f
moises1310M <moises_granados@usmp.pe>
2023-04-27 15:51:10 -0500

    data migration

--- Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -4,7 +4,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
-using test.Data;
+using TPEZANO.Data;
 
 #nullable disable
 
