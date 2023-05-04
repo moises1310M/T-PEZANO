d92004fc357d872e279ad1521327c94b781fe0bf
moises1310M <moises_granados@usmp.pe>
2023-05-04 14:52:01 -0500

    proforma

--- Controllers/ProformaController.cs
+++ Controllers/ProformaController.cs
@@ -25,63 +25,26 @@ namespace TPEZANO.Controllers
             _userManager = userManager;   
         }
 
-        public async Task<IActionResult> Index(){
-            var userID = _userManager.GetUserName(User);
+        public IActionResult Index()
+        {
+            var userIDSession = _userManager.GetUserName(User);
+           
             var items = from o in _context.DataProforma select o;
-
-            items = items.Include(p => p.producto).Where(w => w.UserID.Equals(userID) && w.Status.Equals("PENDIENTE"));
-
-            var carrito = await items.ToListAsync();
-            var total = carrito.Sum(c => c.Quantity*c.Price);
-
-            dynamic model = new ExpandoObject(); 
-            model.montoTotal = total;
-            model.elementosCarrito = carrito;
-
-            return View(model);   
+            items = items.Include(p => p.producto).
+                    Where(w => w.UserID.Equals(userIDSession) &&
+                     w.Status.Equals("PENDIENTE"));
+            var itemsCarrito = items.ToList();
             
-        }
-        // GET: Producto/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+            var total = itemsCarrito.Sum(c => c.Quantity * c.Price);
+            
+            dynamic model = new ExpandoObject();
+            model.montoTotal = total;
+            model.elementosCarrito = itemsCarrito;
 
-            var proforma = await _context.DataProforma
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (proforma == null)
-            {
-                return NotFound();
-            }
-
-            return View(proforma);
+        
+            return View(model);
         }
 
-        // GET: Producto/Create
-        public IActionResult Create()
-        {
-            return View();
-        }
-
-        // POST: Producto/Create
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("UserID,producto,Quantity,Price,Status")] Proforma proforma)
-        {
-            if (ModelState.IsValid)
-            {
-                _context.Add(proforma);
-                await _context.SaveChangesAsync();
-                return RedirectToAction(nameof(Index));
-            }
-            return View(proforma);
-        }
-
-        // GET: Producto/Edit/5
         public async Task<IActionResult> Edit(int? id)
         {
             if (id == null)
@@ -97,12 +60,9 @@ namespace TPEZANO.Controllers
             return View(proforma);
         }
 
-        // POST: Producto/Edit/5
-        // To protect from overposting attacks, enable the specific properties you want to bind to.
-        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("UserID,producto,Quantity,Price,Status")] Proforma proforma)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Quantity,Price,UserID")] Proforma proforma)
         {
             if (id != proforma.Id)
             {
@@ -118,7 +78,7 @@ namespace TPEZANO.Controllers
                 }
                 catch (DbUpdateConcurrencyException)
                 {
-                    if (!ProductoExists(proforma.Id))
+                    if (!_context.DataProforma.Any(e => e.Id == id))
                     {
                         return NotFound();
                     }
@@ -132,7 +92,6 @@ namespace TPEZANO.Controllers
             return View(proforma);
         }
 
-        // GET: Producto/Delete/5
         public async Task<IActionResult> Delete(int? id)
         {
             if (id == null)
@@ -140,30 +99,16 @@ namespace TPEZANO.Controllers
                 return NotFound();
             }
 
-            var proforma = await _context.DataProforma
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (proforma == null)
-            {
-                return NotFound();
-            }
-
-            return View(proforma);
-        }
-
-        // POST: Producto/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(int id)
-        {
             var proforma = await _context.DataProforma.FindAsync(id);
             _context.DataProforma.Remove(proforma);
             await _context.SaveChangesAsync();
             return RedirectToAction(nameof(Index));
         }
 
-        private bool ProductoExists(int id)
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
         {
-            return _context.DataProforma.Any(e => e.Id == id);
+            return View("Error!");
         }
 
         
