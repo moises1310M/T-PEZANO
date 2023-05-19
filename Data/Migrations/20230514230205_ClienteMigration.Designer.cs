025639239866a93f05ca8e846d50bc1b25e8c32d
moises1310M <moises_granados@usmp.pe>
2023-05-18 22:11:00 -0500

    agregando

--- Data/Migrations/20230514230205_ClienteMigration.Designer.cs
+++ Data/Migrations/20230514230205_ClienteMigration.Designer.cs
@@ -2,18 +2,21 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using TPEZANO.Data;
 
 #nullable disable
 
-namespace test.Data.Migrations
+namespace TPEZANO.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230514230205_ClienteMigration")]
+    partial class ClienteMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
