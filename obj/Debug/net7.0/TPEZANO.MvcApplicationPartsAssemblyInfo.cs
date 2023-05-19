16ff6cc06de6983756379bb19bbea007423b2511
moises1310M <moises_granados@usmp.pe>
2023-05-18 22:15:47 -0500

    agregando

--- obj/Debug/net7.0/TPEZANO.MvcApplicationPartsAssemblyInfo.cs
+++ obj/Debug/net7.0/TPEZANO.MvcApplicationPartsAssemblyInfo.cs
@@ -11,6 +11,8 @@ using System;
 using System.Reflection;
 
 [assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Microsoft.AspNetCore.Identity.UI")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("Rotativa.AspNetCore")]
+[assembly: Microsoft.AspNetCore.Mvc.ApplicationParts.ApplicationPartAttribute("RotativaHQ.AspNetCore")]
 
 // Generado por la clase WriteCodeFragment de MSBuild.
 
